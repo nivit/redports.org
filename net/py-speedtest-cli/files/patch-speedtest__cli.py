9a56ebde0928be2fcbb1a901dea407d21bd7b33a
Nicola Stanislao Vitale <nivit@rocketmail.com>
2015-03-07 15:54:00 +0100

    Check if a CA database exists

--- net/py-speedtest-cli/files/patch-speedtest__cli.py
+++ net/py-speedtest-cli/files/patch-speedtest__cli.py
@@ -1,11 +1,15 @@
---- speedtest_cli.py.orig	2015-03-06 10:30:33 UTC
+--- speedtest_cli.py.orig	2015-02-26 17:44:18 UTC
 +++ speedtest_cli.py
-@@ -754,6 +754,8 @@ def speedtest():
+@@ -754,6 +754,12 @@ def speedtest():
  
  
  def main():
-+    if 34015728 <= sys.hexversion and not 'SSL_CERT_FILE' in os.environ:
-+        os.environ['SSL_CERT_FILE'] = '%%LOCALBASE%%/share/certs/ca-root-nss.crt'
++    if 34015728 <= sys.hexversion:
++        from setuptools import ssl_support as ssl_support
++        ssl_cert_file = ssl_support.find_ca_bundle()
++        if not ssl_cert_file and not 'SSL_CERT_FILE' in os.environ:
++            os.environ['SSL_CERT_FILE'] = '%%LOCALBASE%%/share/certs/ca-root-nss.crt'
++
      try:
          speedtest()
      except KeyboardInterrupt:
